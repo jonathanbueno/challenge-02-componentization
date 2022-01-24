00f99d80fe69dc8f05e4f57fcb1d94399af5b13b
Jonathan Bueno <jfb1601@hotmail.com>
2022-01-24 21:29:46 +0000

    Migrated types to the types file. Initial Componentization for Content

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -1,1 +1,23 @@
-declare module '*.svg'
+declare module '*.svg';
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export interface ContentProps {
+  movies: Array<MovieProps>;
+  selectedGenre: GenreResponseProps;
+}
